726c06fb269f9484451e0499b20637844a7182d6
sumanstm41 <suman.magar@patientsky.com>
2022-05-25 10:10:43 +0200

    Navigation structure file and Test Screens

---
+++ src/Navigation/MainNavigation.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {Text, SafeAreaView} from 'react-native';
+import React from 'react';
+import BottomNavigation from './BottomNavigation';
+
+const MainNavigation = () => {
+  return (
+    // <SafeAreaView>
+    //   <Text>MainNavigation</Text>
+    <BottomNavigation />
+    // </SafeAreaView>
+  );
+};
+
+export default MainNavigation;
